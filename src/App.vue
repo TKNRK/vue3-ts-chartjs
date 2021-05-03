56550e7ccfa9f0a29872fab092ea6e09d43805ed
Riku Takano <rick.takano@gmail.com>
2021-05-03 15:44:04 +0900

    add sample time line chart

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/line-chart">LineChart</router-link> |
     <router-link to="/line-chart/random">LineChart(Random)</router-link> |
+    <router-link to="/line-chart/time">TimeLineChart</router-link> |
     <router-link to="/line-chart/multi-axis">MultiAxisLineChart</router-link> |
     <router-link to="/line-chart/styled">StyledLineChart</router-link>
   </div>
